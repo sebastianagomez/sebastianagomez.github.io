4c91ab504660f336e9fc4d789916fc1337b134a5
Sebastián Gómez <sebastian.a.gomez@outlook.com.ar>
2021-01-16 22:37:39 -0300

    Cuarto Cambio

---
+++ lesson-1/js1/script1.js
@@ -0,0 +1,41 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let d = new Date();
+  let dayName = daynames[d.getDay()];
+  let monthName = months[d.getMonth()];
+  let fulldate =
+    dayName +
+    ", " +
+    monthName +
+    " " +
+    d.getDate() +
+    ", " +
+    d.getFullYear();
+
+  document.getElementById("currentdate").textContent = fulldate;
+
+  document.getElementById("currentyear").textContent = d.getFullYear();
+
+  let oLastModif = new Date(document.lastModified);
+  document.getElementById("lastModified").textContent = oLastModif;
