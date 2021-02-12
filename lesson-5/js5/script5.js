b52be70ded6f2c36b1afb8b9fa978027ede0a28b
Sebastián Gómez <sebastian.a.gomez@outlook.com.ar>
2021-02-12 13:19:22 -0300

    change date js

--- lesson-5/js5/script5.js
+++ lesson-5/js5/script5.js
@@ -44,3 +44,19 @@ let daynames = [
 function toggleMenu(){
 document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+function dateBanner() {
+  var d = new Date();
+  var n = d.getDay()
+  if(n === 5) {
+      var element = document.getElementById("announement")
+      element.classList.add("showBanner");
+      return;
+  }
+  else {
+      var element = document.getElementById("announement")
+      element.classList.add("closeBanner");
+      return;
+  }
+  
+}
