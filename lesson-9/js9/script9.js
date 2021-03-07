821c3f75d858a3c7ceb299022c95dd66f328403a
Sebastián Gómez <sebastian.a.gomez@outlook.com.ar>
2021-03-06 22:54:18 -0300

    1st commit lesson 9

--- lesson-9/js9/script9.js
+++ lesson-9/js9/script9.js
@@ -89,7 +89,7 @@ images.forEach((image) => {
   imgObserver.observe(image);
 });
 
-//-------- FORM --------------//
+//-------- FORM --------------
 
 function adjustRating(rating) {
   document.getElementById("ratingvalue").innerHTML = rating;
@@ -103,3 +103,61 @@ s.textContent = sel.value;
 
 }
 
+// ---------- JSON IMAGES-------------
+
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+
+.then(function (jsonObject) {
+    // check for json object
+    // console.table(jsonObject);
+
+const towns = jsonObject['towns'];
+    
+// loop through the array
+for (let i=0; i < towns.length; i++) {
+    // declare each variable
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let motto = document.createElement('h3');
+    let year = document.createElement('p');
+    let pop = document.createElement('p');
+    let rain = document.createElement('p');
+    let image = document.createElement('img');
+    let textd = document.createElement('div')
+    
+    // What each card will have, contatenation of the strings, declaring classes to make CSS easier
+    if (towns[i].name == 'Preston'|| towns[i].name == 'Fish Haven' || towns[i].name == 'Soda Springs') {
+    
+    h2.textContent = towns[i].name;
+    motto.textContent = "Town Motto:" + " " + towns[i].motto;    
+    year.textContent = "Year Established:" + " " + towns[i].yearFounded;
+    pop.textContent = "Current Population:" + " " + towns[i].currentPopulation;
+    rain.textContent = "Annual Rainfall:" + " " +  towns[i].averageRainfall;
+    image.setAttribute('src', `img/${towns[i].photo}`);
+    image.setAttribute('alt', towns[i].name);
+    image.setAttribute('class', 'townimg');
+    card.setAttribute('class', "home_sect" );
+    textd.setAttribute('class', 'home_town_text');
+
+    // build the display by updating as I loop through. Use an "if" statement to select just the towns I want.
+    // if (towns[i].name == 'Preston'|| towns[i].name == 'Fish Haven' || towns[i].name == 'Soda Springs') {
+        card.appendChild(textd);
+        textd.appendChild(h2);
+        textd.appendChild(motto);
+        textd.appendChild(year);
+        textd.appendChild(pop);
+        textd.appendChild(rain);
+        card.appendChild(image);}
+
+    else {
+        card.setAttribute('class', 'home_hide');
+    }
+        
+     document.querySelector('div.town_div').appendChild(card);
+}
+});
