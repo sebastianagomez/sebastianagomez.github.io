5d71ea167735716886775fef7509bde31a2aed91
Sebastián Gómez <sebastian.a.gomez@outlook.com.ar>
2021-03-13 23:14:02 -0300

    First Commit for Final Project W10

--- lesson-10/js10/script10.js
+++ lesson-10/js10/script10.js
@@ -103,61 +103,97 @@ s.textContent = sel.value;
 
 }
 
-// ---------- JSON IMAGES-------------
+// ------- WEATHER API -----------
 
-const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+//CURRENT WEATHER DATA
+// Give JS the address of the weather API with city code, imperial units, and APPID
 
-fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=31faaedc2de9ab98c63a927fb8525e85";
 
-.then(function (jsonObject) {
-    // check for json object
-    // console.table(jsonObject);
+// Use Fetch to retrieve apiURL. Convert string to json object. Print to console log to check it works and evaluate what data is available to use.
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
 
-const towns = jsonObject['towns'];
-    
-// loop through the array
-for (let i=0; i < towns.length; i++) {
-    // declare each variable
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let motto = document.createElement('h3');
-    let year = document.createElement('p');
-    let pop = document.createElement('p');
-    let rain = document.createElement('p');
-    let image = document.createElement('img');
-    let textd = document.createElement('div')
-    
-    // What each card will have, contatenation of the strings, declaring classes to make CSS easier
-    if (towns[i].name == 'Preston'|| towns[i].name == 'Fish Haven' || towns[i].name == 'Soda Springs') {
-    
-    h2.textContent = towns[i].name;
-    motto.textContent = "Town Motto:" + " " + towns[i].motto;    
-    year.textContent = "Year Established:" + " " + towns[i].yearFounded;
-    pop.textContent = "Current Population:" + " " + towns[i].currentPopulation;
-    rain.textContent = "Annual Rainfall:" + " " +  towns[i].averageRainfall;
-    image.setAttribute('src', `img/${towns[i].photo}`);
-    image.setAttribute('alt', towns[i].name);
-    image.setAttribute('class', 'townimg');
-    card.setAttribute('class', "home_sect" );
-    textd.setAttribute('class', 'home_town_text');
+    // assign js Objects to elements by ID. Pay close attention to which data is in an array.
+    document.getElementById("cond").textContent = jsObject.weather[0].main;
+    document.getElementById("temp").textContent = Math.round(
+      jsObject.main.temp
+    );
+    document.getElementById("speed").textContent = Math.round(
+      jsObject.wind.speed
+    );
+    document.getElementById("humid_pct").textContent =
+      jsObject.main.humidity + "%";
 
-    // build the display by updating as I loop through. Use an "if" statement to select just the towns I want.
-    // if (towns[i].name == 'Preston'|| towns[i].name == 'Fish Haven' || towns[i].name == 'Soda Springs') {
-        card.appendChild(textd);
-        textd.appendChild(h2);
-        textd.appendChild(motto);
-        textd.appendChild(year);
-        textd.appendChild(pop);
-        textd.appendChild(rain);
-        card.appendChild(image);}
+    // Use weather API data to calculate wind chill. Be sure to parseFloat to convert string data to numbers.
+    let t = jsObject.main.temp;
+    let w = jsObject.wind.speed;
 
-    else {
-        card.setAttribute('class', 'home_hide');
+    if (t <= 50 && w >= 3) {
+      let wc =
+        35.74 +
+        0.6215 * t -
+        35.75 * Math.pow(w, 0.16) +
+        0.4275 * t * Math.pow(w, 0.16);
+
+      wc = document.getElementById("windchill_num").innerHTML =
+        Math.round(wc) + "&deg; " + "F";
+    } else {
+      wc = "Not Applicable";
+
+      document.getElementById("windchill_num").innerHTML = wc;
     }
-        
-     document.querySelector('div.town_div').appendChild(card);
-}
+  });
+
+  //FIVE DAY FORECAST DATA
+
+// List the URL of the API forecast data
+const apiforecastURL =
+"https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=31faaedc2de9ab98c63a927fb8525e85";
+
+// As before, fetch the forecast data, create the JSON object, console log to make sure it worked.
+fetch(apiforecastURL)
+.then((response) => response.json())
+.then((jsObject) => {
+  // Create an array with day names to use for five day forecast
+  const dayName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  // Filter the jsObject to only show data from 6:00 pm (18:00:00)
+  const thefive = jsObject.list.filter((element) =>
+    element.dt_txt.includes("18:00:00")
+  );
+
+  // Use a "for" loop to loop through the data and fill the table
+  let day = 0;
+  let i = 0;
+
+  for (i = 0; i < thefive.length; i++) {
+    let d = new Date(thefive[i].dt_txt); //date object to get date
+
+    //write day name using array built earlier
+    document.getElementById("dayofweek" + (day + 1)).textContent =
+      dayName[d.getDay()];
+
+    // write temperature data. Use Math.round to make it a whole number.
+    document.getElementById("forecast" + (day + 1)).textContent =
+      Math.round(thefive[day].main.temp) + " °F";
+
+    // give the browser the icon address
+    var imagesrc =
+      "https://openweathermap.org/img/w/" +
+      thefive[day].weather[0].icon +
+      ".png";
+
+    // populate table with icons, set alt attribute using weather description from the object
+    document.getElementById("imagesrc" + (day + 1)).textContent = imagesrc;
+    document.getElementById("icon" + (day + 1)).setAttribute("src", imagesrc);
+    document
+      .getElementById("icon" + (day + 1))
+      .setAttribute("alt", thefive[0].weather[0].description);
+
+    day++;
+  }
 });
